032c7e2ebb2e7f15708a47bf296cc7ec3580abe4
john-ngugi <johnngugi0407@gmail.com>
2023-08-28 10:34:26 +0300

    initial commit

--- maps/urls.py
+++ maps/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('',views.index ,name ="home")
+    path('',views.index ,name ="home"),
+    path('layers/',views.getLayers, name="layersPage")
 ]
 
 if settings.DEBUG:
